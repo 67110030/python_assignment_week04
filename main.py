230caa173aef3cd272bd5c3d1a39e97f6c61e62a
67110030 <67110030@kmitl.ac.th>
2025-07-21 16:03:25 +0700

    5

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import string_operations as so
+from utilities import string_operations as so, calculator as cal 
 def main():
+    
     sample_string = "hello World"
     print("Original String:", sample_string)
     print("Reversed String:", so.reverse_string(sample_string))
@@ -7,5 +8,9 @@ def main():
     print("Lowercase String:", so.lowercase_string(sample_string))
     print("Uppercase String:", so.uppercase_string(sample_string))
 
+    print("Addition:", cal.add(5, 3))
+    print("Subtraction:", cal.subtract(10, 4))
+    print("Multiplication:", cal.multiply(6, 7))
+    print("Division:", cal.divide(8, 2))
 main()
     
